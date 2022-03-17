8f9ddf24e55ce5b7b5f09a9a4b9df871de3314ea
MaksDim <demasig@yandex.ru>
2022-03-17 09:15:40 +0400

    First coomit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+                int coast = 5000;
+                int part = 20;
+                int bonus = coast / part;
+        return bonus;
+    }
+}
